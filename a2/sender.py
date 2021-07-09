7ad2f2d7fad4a34597e5caa03e5a8e6990fc3754
Kejian Zhu <k36zhu@uwaterloo.ca>
2021-07-09 18:05:03 -0400

    A2: sender support buffer

--- a2/sender.py
+++ a2/sender.py
@@ -22,24 +22,32 @@ class server_proc:
             print("Valid type of arguements should be provided")
 
         self.last_ack = -1
-        self.ack_lock = threading.Lock()
-        self.wdn_sem = threading.Semaphore(1)
+        self.last_send = 0
+        self.wdn_size = 1
+        self.wdn_sem = threading.Semaphore(self.wdn_size)
         self.ackSocket = s.socket(AF_INET, SOCK_DGRAM)
+        self.sendSocket = s.socket(AF_INET, SOCK_DGRAM)   
+        self.finish = False
+        self.buffer = [None for _ in range(32)]
+        self.timer_started = False
+
         self.ackSocket.bind(('', self.ack_port))
-        self.sendSocket = s.socket(AF_INET, SOCK_DGRAM)
         self.sendSocket.bind(('', 0))
 
     def send_thread(self):
-        couter = 0
         while True:
-            chunk = self.file.read(50)
-            if not chunk:
+            chunk = self.file.read(5)
+            if not chunk:   # reach the EOF
                 break
-            self.wdn_sem.acquire()
-            packet = self.cons_packet(1, couter % 32, chunk)
+            self.wdn_sem.acquire()  # if there are avalible space in sender window
+            packet = self.cons_packet(1, self.last_send, chunk)
+            self.buffer[self.last_send] = packet
+            if not self.timer_started:
+                self.timer_started = True
+                self.start_time = time.time()
             self.sendSocket.sendto(packet, (self.emu_add, self.emu_port))
-            couter += 1
-        packet = self.cons_packet(2, 0, '')
+            self.last_send = (self.last_send + 1) % 32
+        packet = self.cons_packet(2, 0, '') # send EOT packet
         self.sendSocket.sendto(packet, (self.emu_add, self.emu_port))
     
     def ack_thread(self):
@@ -47,16 +55,28 @@ class server_proc:
             ack_msg, sadd = self.ackSocket.recvfrom(1024)
 
             p_type, ack_seq, length, payload = self.process_packet(ack_msg)
-            print(p_type)
-            if p_type == 0:
-                for i in range(0, ack_seq - self.last_ack):
+            if p_type == 0: # ACK message
+                for i in range(min(ack_seq - self.last_ack,self.wdn_size)):
                     self.wdn_sem.release()
+                if self.wdn_size < 10:
+                    self.wdn_size += 1
+                    self.wdn_sem.release() # increment window size by 1
                 self.last_ack = ack_seq
-            else:
+                if self.last_send == self.last_ack:
+                    self.timer_started = False
+                else:
+                    self.start_time = time.time()
+            else:   # receiver receive the EOT message and response with another EOT message
+                self.finish = True  # tell timer to terminate
                 break
     
     def timer_thread(self):
-        print()
+        while(True):
+            if self.finish:
+                break
+            if self.timer_started and time.time() - self.start_time > self.timeout: # timeout occures
+                self.resend()
+        
 
     def run(self):
         if not self.valid:
@@ -70,10 +90,19 @@ class server_proc:
         # create thread for sending packet, receiving acknowledgement and timer
         send_t = threading.Thread(target=self.send_thread)
         ack_t = threading.Thread(target=self.ack_thread)
+        timer_t = threading.Thread(target=self.timer_thread)
         send_t.start()
         ack_t.start()
+        timer_t.start()
         send_t.join()
         ack_t.join()
+        timer_t.join()
+    
+    def resend(self):
+        self.sendSocket.sendto(self.buffer[self.last_ack + 1], (self.emu_add, self.emu_port))
+        self.wdn_size = 1
+        self.wdn_sem = threading.Semaphore(0)   # reset window size to 1, and because we resend the timeout packet, reduce avaliable space to 0
+        self.start_time = time.time()
 
     def process_packet(self, byte_stream):
         p_type = int.from_bytes(byte_stream[0:4], byteorder='little', signed=False)
